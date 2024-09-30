0744620d46b0143d335dd9b1b25504029e53d618
Olivia Lu <oliviayu@google.com>
2024-09-30 11:30:24 +0800

    PostCreate and PostList UI

---
+++ client/src/PostList.js
@@ -0,0 +1,33 @@
+import React,{useState,useEffect} from "react";
+import axios from 'axios'
+const PostList = ()=>{
+    // posts = {
+                // "3559e7df": {
+                //     "id": "3559e7df",
+                //     "title": "123"
+                // },
+                // "fb472456": {
+                //     "id": "fb472456",
+                //     "title": "second post"
+                // }
+            // }
+    const [posts,setPosts] = useState([])
+    const fetchPost = async()=>{
+        const res = await axios.get('http://localhost:4000/posts')
+        setPosts(res.data)
+    }
+    useEffect(()=>{fetchPost()},[])
+    const renderedPosts = Object.values(posts).map(post=>{
+        return(
+            <div className="card" style={{width:'30%',marginBottom:'20px'}} key={[post.id]}>
+                <div className="card-body"><h2>{post.title}</h2></div>         
+            </div>
+        )
+    })
+    return(
+        <div className="d-flex flex-row flex-wrap justify-content-between">
+            {renderedPosts}
+        </div>
+    )
+}
+export default PostList
