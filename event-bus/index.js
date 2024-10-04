12cc3d27299da490fdff3868f472fb3d641b618b
Yu-Hsuan Lu <98661228+YuHsuan-Lu@users.noreply.github.com>
2024-10-04 23:27:45 +0800

    wire up pod/event-bus and pod/posts by revise ip address in event-bus/index.js and posts/index.js

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,32 +1,33 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const axios = require('axios')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const storedEvent = []
-app.post('/events',(req,res)=>{
-    const event = req.body
-    storedEvent.push(event)
+const storedEvent = [];
+app.post("/events", (req, res) => {
+  const event = req.body;
+  storedEvent.push(event);
 
-    axios.post('http://localhost:4000/events',event).catch(err=>{
-        console.log(err.message)
-    })
-    axios.post('http://localhost:4001/events',event).catch(err=>{
-        console.log(err.message)
-    })
-    axios.post('http://localhost:4002/events',event).catch(err=>{
-        console.log(err.message)
-    })
-    axios.post('http://localhost:4003/events',event).catch(err=>{
-        console.log(err.message)
-    })
-    res.send({status: 'ok'})
-})
-app.get('/events',(req,res)=>{
-    res.send(storedEvent)
-})
-app.listen(4005,()=>{
-    console.log('listening on 4005')
-})
+  // axios.post('http://localhost:4000/events',event).catch(err=>{
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  // axios.post('http://localhost:4001/events',event).catch(err=>{
+  //     console.log(err.message)
+  // })
+  // axios.post('http://localhost:4002/events',event).catch(err=>{
+  //     console.log(err.message)
+  // })
+  // axios.post('http://localhost:4003/events',event).catch(err=>{
+  //     console.log(err.message)
+  // })
+  res.send({ status: "ok" });
+});
+app.get("/events", (req, res) => {
+  res.send(storedEvent);
+});
+app.listen(4005, () => {
+  console.log("listening on 4005");
+});
--- posts/index.js
+++ posts/index.js
@@ -19,7 +19,8 @@ app.post("/posts", async (req, res) => {
   const { title } = req.body;
   posts[id] = { id, title };
   // emit an event
-  await axios.post("http://localhost:4005/events", {
+  // await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-clusterip-srv:4005/events", {
     type: "PostCreated",
     data: { id, title },
   });
